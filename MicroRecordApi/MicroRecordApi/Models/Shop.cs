829719808e6d04dd64611c1c1d9f2874a05a4edf
zhenyugu <erickool1992@live.com>
2017-10-10 22:38:30 +0800

    add shop class

---
+++ MicroRecordApi/MicroRecordApi/Models/Shop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MicroRecordApi.Models
+{
+    public class Shop
+    {
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
